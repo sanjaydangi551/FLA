366ed16ebcf58894504ca7765c7ee703e186e661
markdmy <markdmy@hotmail.com>
2023-09-10 11:59:25 -0400

    added links to nav in index.html

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
 // program.html
 
-const menuTitles = document.querySelectorAll('.menu-title');
-
-menuTitles.forEach(title => {
-    title.addEventListener('click', () => {
-        const content = title.nextElementSibling;
-
-        // Toggle the content visibility
-        if (content.style.display === 'block') {
-            content.style.display = 'none';
-            title.querySelector('.icon').textContent = '+';
-        } else {
-            content.style.display = 'block';
-            title.querySelector('.icon').textContent = '-';
-        }
-    });
-});
-
-
 document.addEventListener("DOMContentLoaded", function () {
-    const hamburgerIcon = document.querySelector(".hamburger-icon");
-    const navMobile = document.querySelector(".navMobile");
+  // Select all menu titles
+  const menuTitles = document.querySelectorAll('.menu-title');
 
-    hamburgerIcon.addEventListener("click", function (event) {
-        event.stopPropagation();
-        navMobile.classList.toggle("visible");
+  // Function to toggle menu visibility
+  function toggleMenu(content, title) {
+    // Toggle the content visibility
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+      title.querySelector('.icon').textContent = '+';
+    } else {
+      content.style.display = 'block';
+      title.querySelector('.icon').textContent = '-';
+    }
+  }
 
-        // Disable scrolling when the menu is visible
-        if (navMobile.classList.contains("visible")) {
-            document.body.style.overflow = "hidden";
-        } else {
-            document.body.style.overflow = "auto";
-        }
+  // Loop through menu titles and add click event listeners
+  menuTitles.forEach(title => {
+    title.addEventListener('click', () => {
+      const content = title.nextElementSibling;
+      toggleMenu(content, title);
     });
+  });
 
-    document.addEventListener("click", function (event) {
-        if (!navMobile.contains(event.target) && !hamburgerIcon.contains(event.target)) {
-            navMobile.classList.remove("visible");
-            document.body.style.overflow = "auto"; 
-        }
-    });
+  // Check if there is a hash in the URL (e.g., #eligible)
+  if (window.location.hash) {
+    // Remove the '#' character from the hash
+    const hash = window.location.hash.substring(1);
+
+    // Find the menu with the corresponding ID
+    const menu = document.getElementById(hash);
+
+    // If a matching menu is found, open it
+    if (menu) {
+      const content = menu.querySelector('.menu-content');
+      const title = menu.querySelector('.menu-title');
+      toggleMenu(content, title);
+    }
+  }
 });
 
 let slideIndex = 0;
