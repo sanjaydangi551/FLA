db6583a185fc86f79a871fbd4eb0ecaad7d72d03
markdmy <markdmy@hotmail.com>
2023-09-11 11:44:49 -0400

    indentation added
    pictures renamed, donation bar static

--- contact.js
+++ contact.js
@@ -1,28 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const contactForm = document.getElementById("contact-form");
+  const contactForm = document.getElementById("contact-form");
 
-    contactForm.addEventListener("submit", function (e) {
-        e.preventDefault();
+  contactForm.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-        const formData = new FormData(contactForm);
-        formData.append("recipient", "markdmy@hotmail.com");
+    const formData = new FormData(contactForm);
+    formData.append("recipient", "markdmy@hotmail.com");
 
-        fetch("your-server-script-url.php", {
-            method: "POST",
-            body: formData,
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.success) {
-                    alert("Message sent successfully!");
-                    contactForm.reset();
-                } else {
-                    alert("Message failed to send. Please try again later.");
-                }
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-                alert("An error occurred. Please try again later.");
-            });
-    });
+    fetch("your-server-script-url.php", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          alert("Message sent successfully!");
+          contactForm.reset();
+        } else {
+          alert("Message failed to send. Please try again later.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("An error occurred. Please try again later.");
+      });
+  });
 });
